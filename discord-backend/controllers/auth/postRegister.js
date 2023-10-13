7231ecfb2786c1689cdb9d8f363fdcfbbe11dc80
Mahmoud Mehisen <mahmoud.mehisen@gmail.com>
2023-10-14 00:37:24 +0300

    add register api

--- discord-backend/controllers/auth/postRegister.js
+++ discord-backend/controllers/auth/postRegister.js
@@ -1,5 +1,37 @@
+const User = require('../../models/user')
+const bcrypt = require("bcryptjs")
 const postRegister = async (req, res) => {
-  res.send("register route");
+    try {
+        const {username, email, password} = req.body;
+
+        const userExists = await User.exists({email})
+
+        if (userExists) {
+            return res.status(409).send("E-mail already in use.");
+        }
+
+        const encryptedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            username,
+            email: email.toLowerCase(),
+            password: encryptedPassword,
+        })
+
+        const token = 'JWT TOKEN';
+
+        res.status(201).json({
+            user: {
+                email: user.email,
+                token: token,
+                username: username
+            }
+        })
+
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send("Error occured. Please try again");
+    }
 };
 
 module.exports = postRegister;
