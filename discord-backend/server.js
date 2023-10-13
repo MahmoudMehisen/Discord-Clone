2e2b88c1a4e4e3898ce3d5d11b8db036e7facf24
Mahmoud Mehisen <mahmoud.mehisen@gmail.com>
2023-10-13 17:11:04 +0300

    add auth url

--- discord-backend/server.js
+++ discord-backend/server.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const authRoutes = require("./routes/authRoutes");
+
 const PORT = process.env.PORT || process.env.API_PORT;
 
 const app = express();
@@ -11,16 +13,20 @@ const app = express();
 app.use(express.json);
 app.use(cors());
 
+app.use("/api/auth", authRoutes);
+
 const server = http.createServer(app);
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    server.listen(PORT, () => {
-      console.log(`Server is listening on ${PORT}`);
-    });
+    console.log("database connected");
   })
   .catch((err) => {
     console.log("database connection faild. Server not started");
     console.log(err);
   });
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
+});
