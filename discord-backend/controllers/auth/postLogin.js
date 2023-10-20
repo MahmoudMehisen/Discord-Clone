b80fe37eb78ee546620d681601039191e1d83de1
Mahmoud Mehisen <mahmoud.mehisen@gmail.com>
2023-10-20 15:40:55 +0300

    add jwt to auth

--- discord-backend/controllers/auth/postLogin.js
+++ discord-backend/controllers/auth/postLogin.js
@@ -1,27 +1,33 @@
 const User = require("../../models/user");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken")
 const postLogin = async (req, res) => {
-    try {
-        const {email, password} = req.body;
+  try {
+    const { email, password } = req.body;
 
-        const user = await User.findOne({email: email.toLowerCase()})
+    const user = await User.findOne({ email: email.toLowerCase() });
 
-        if (user && (await bcrypt.compare(password, user.password))) {
-            const token = 'JWT TOKEN';
-            return res.status(201).json({
-                user: {
-                    email: user.email,
-                    token: token,
-                    username: user.username
-                }
-            })
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const token = jwt.sign(
+        { userId: user._id, email: user.email },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "24h",
         }
-        return res.status(400).send("Invalid credentials. Please try again")
-
-    } catch (err) {
-        console.log(err);
-        return res.status(500).send("Error occurred. Please try again");
+      );
+      return res.status(201).json({
+        user: {
+          email: user.email,
+          token: token,
+          username: user.username,
+        },
+      });
     }
+    return res.status(400).send("Invalid credentials. Please try again");
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("Error occurred. Please try again");
+  }
 };
 
 module.exports = postLogin;
--- discord-backend/controllers/auth/postRegister.js
+++ discord-backend/controllers/auth/postRegister.js
@@ -1,37 +1,43 @@
-const User = require('../../models/user')
-const bcrypt = require("bcryptjs")
+const User = require("../../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const postRegister = async (req, res) => {
-    try {
-        const {username, email, password} = req.body;
+  try {
+    const { username, email, password } = req.body;
 
-        const userExists = await User.exists({email})
+    const userExists = await User.exists({ email });
 
-        if (userExists) {
-            return res.status(409).send("E-mail already in use.");
-        }
-
-        const encryptedPassword = await bcrypt.hash(password, 10);
-
-        const user = await User.create({
-            username,
-            email: email.toLowerCase(),
-            password: encryptedPassword,
-        })
-
-        const token = 'JWT TOKEN';
-
-        return res.status(201).json({
-            user: {
-                email: user.email,
-                token: token,
-                username: username
-            }
-        })
-
-    } catch (err) {
-        console.log(err);
-        return res.status(500).send("Error occurred. Please try again");
+    if (userExists) {
+      return res.status(409).send("E-mail already in use.");
     }
+
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      username,
+      email: email.toLowerCase(),
+      password: encryptedPassword,
+    });
+
+    const token = jwt.sign(
+      { userId: user._id, email: user.email },
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: "24h",
+      }
+    );
+
+    return res.status(201).json({
+      user: {
+        email: user.email,
+        token: token,
+        username: username,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("Error occurred. Please try again");
+  }
 };
 
 module.exports = postRegister;
