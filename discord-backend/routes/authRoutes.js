ef8695c7b83f75ca3253674be183f00c77558192
Mahmoud Mehisen <mahmoud.mehisen@gmail.com>
2023-10-20 15:52:24 +0300

    verify jwt aujth

--- discord-backend/routes/authRoutes.js
+++ discord-backend/routes/authRoutes.js
@@ -3,28 +3,32 @@ const router = express.Router();
 const authControllers = require("../controllers/auth/authControllers");
 const Joi = require("joi");
 const validator = require("express-joi-validation").createValidator({});
-
+const auth = require('../middleware/auth')
 const registerSchema = Joi.object({
-  username: Joi.string().min(3).max(12).required(),
-  password: Joi.string().min(6).max(12).required(),
-  email: Joi.string().email().required(),
+    username: Joi.string().min(3).max(12).required(),
+    password: Joi.string().min(6).max(12).required(),
+    email: Joi.string().email().required(),
 });
 
 const loginSchema = Joi.object({
-  password: Joi.string().min(6).max(12).required(),
-  email: Joi.string().email().required(),
+    password: Joi.string().min(6).max(12).required(),
+    email: Joi.string().email().required(),
 });
 
 router.post(
-  "/register",
-  validator.body(registerSchema),
-  authControllers.controllers.postRegister
+    "/register",
+    validator.body(registerSchema),
+    authControllers.controllers.postRegister
 );
 
 router.post(
-  "/login",
-  validator.body(loginSchema),
-  authControllers.controllers.postLogin
+    "/login",
+    validator.body(loginSchema),
+    authControllers.controllers.postLogin
 );
+
+router.get("/test", auth, (req, res) => {
+  res.send("request passed")
+})
 
 module.exports = router;
